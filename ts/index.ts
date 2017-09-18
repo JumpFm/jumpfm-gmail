1335438c4ecf3899a62233f708d444cd52511182
Gilad Kutiel <gilad.kutiel@gmail.com>
2017-09-18 18:06:24 +0300

    foo

--- ts/index.ts
+++ ts/index.ts
@@ -20,14 +20,31 @@ export const load = (jumpfm: JumpFm) => {
         return res
     }
 
-    new Auth().getToken().then(auth => {
-        const gmail = new Gmail(auth)
-        gmail.query('filename:pdf', 10)
-            .then(msgs => {
-                msgs.forEach(msg => {
-                    gmail.get(msg.id)
-                        .then(res => console.log(getAttachments(res))
-                })
+    jumpfm.panels.forEach(panel => {
+        panel.onCd(() => {
+            if (panel.getUrl().protocol != 'gmail') return
+            new Auth().getToken().then(auth => {
+                const gmail = new Gmail(auth)
+                gmail.query('filename:pdf', 10)
+                    .then(msgs => {
+                        msgs.forEach(msg => {
+                            gmail.get(msg.id)
+                                .then(res => getAttachments(res))
+                                .then(as => panel.setItems(as.map(a => ({
+                                    path: a.id
+                                    , name: a.name
+                                }))))
+                        })
+                    })
             })
+        })
+
+        panel.bind('gmail', ['g'], () => {
+            panel.cd({
+                protocol: 'gmail'
+                , path: '/'
+                , query: {}
+            })
+        })
     })
 }
